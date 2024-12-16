064882e9520d293f42a60bc8f030ed0f70819be0
Lucky7 <mihaitibrea2@gmail.com>
2024-12-16 10:47:25 +0200

    Migrating to Angular Modules

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+platformBrowser().bootstrapModule(AppModule)
